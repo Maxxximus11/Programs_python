7e1b7a1e80f6cac0052278cfb048de6545a26cab
Maxxximus11 <maxboyko1221@gmail.com>
2024-09-11 14:44:51 +0300

    change

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("Hello")
+from tkinter import *
+
+window = Tk() 
+window.geometry("420x420")
+window.title("Nice")
+
+#icon = PhotoImage(file='logo.png')
+#window.iconphoto(True,icon)
+# window.config(background="#5cfcff")
+
+window.mainloop() 
